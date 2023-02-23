4a5d0f9c8dc033b22ee2f258140dcfec38a9549d
Kory Becker <68331399+korybecker@users.noreply.github.com>
2023-02-23 18:28:16 -0500

    added like and unlike handlers

--- src/pages/Sounds.tsx
+++ src/pages/Sounds.tsx
@@ -9,19 +9,57 @@ import { useAuthContext } from "../hooks/useAuthContext";
 import Player from "../components/Player";
 
 const Sounds = () => {
-    const [sounds, setSounds] = useState<Sound[] | null>(null);
-
+    const [sounds, setSounds] = useState<Sound[]>([]);
+    const [likes, setLikes] = useState<string[]>([]);
     const { user } = useAuthContext();
 
     const fetchData = async () => {
         if (sounds && sounds.length > 0) return sounds;
-        const { data } = await axios.get(`${environment.API_URL}/api/v1/sound`);
-        setSounds(data);
+        const res = await axios.get(`${environment.API_URL}/api/v1/sound`);
+        setSounds(res.data);
+        if (user) {
+            const { data } = await axios.get(
+                `${environment.API_URL}/api/v1/like/${user.userId}`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${user.token}`,
+                    },
+                }
+            );
+            setLikes(data);
+        }
     };
 
     useEffect(() => {
         fetchData();
-    }, []);
+    }, [user]);
+
+    const handleLike = (soundId: string) => {
+        setSounds((prevSounds) => {
+            const updatedSounds = prevSounds.map((sound) => {
+                if (sound._id === soundId) {
+                    return { ...sound, likes: sound.likes + 1 };
+                }
+                return sound;
+            });
+            return updatedSounds;
+        });
+
+        setLikes((prevLikes) => [...prevLikes, soundId]);
+    };
+
+    const handleUnlike = (soundId: string) => {
+        setSounds((prevSounds) => {
+            const updatedSounds = prevSounds.map((sound) => {
+                if (sound._id === soundId) {
+                    return { ...sound, likes: sound.likes - 1 };
+                }
+                return sound;
+            });
+            return updatedSounds;
+        });
+        setLikes((prevLikes) => prevLikes.filter((id) => id !== soundId));
+    };
 
     return (
         <>
@@ -31,12 +69,15 @@ const Sounds = () => {
                 {sounds &&
                     sounds.map((sound, i) => (
                         <Player
+                            liked={likes.includes(sound._id)}
                             key={i}
                             sound={sound}
                             username=""
                             setSounds={setSounds}
                             loggedInUserId={user ? user.userId : ""}
                             userToken={user ? user.token : ""}
+                            onLike={() => handleLike(sound._id)}
+                            onUnlike={() => handleUnlike(sound._id)}
                         />
                     ))}
             </div>
