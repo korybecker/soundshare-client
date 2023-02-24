b01395f0938b5603defb58530efef9b3784a021c
Kory Becker <68331399+korybecker@users.noreply.github.com>
2023-02-23 22:50:26 -0500

    merged MyProfile and UserProfile into one component

--- src/pages/Profile/UserProfile.tsx
+++ src/pages/Profile/UserProfile.tsx
@@ -4,55 +4,43 @@ import { environment } from "../../environment";
 import "./styles.css";
 
 import axios from "axios";
-import Player from "../../components/Player";
-import { Sound } from "../../interfaces/Sound";
+import SoundsList from "../../components/SoundsList";
+import { User } from "../../interfaces/User";
+import { useAuthContext } from "../../hooks/useAuthContext";
 
 const UserProfile = () => {
     const { username } = useParams();
-    if (!username) {
-        return <h1>No user</h1>;
-    }
-    const [user, setUser] = useState<{
-        name: string;
-        pfpUrl: string;
-        userId?: string;
-        username: string;
-        token?: string;
-    }>();
-    const [sounds, setSounds] = useState<Sound[]>();
+    const { user: authUser } = useAuthContext();
+
+    const [user, setUser] = useState<User | null>(null);
     useEffect(() => {
-        axios
-            .get(`${environment.API_URL}/api/v1/user/${username}`)
-            .then((res) => {
-                const { name, pfpUrl } = res.data;
-                setUser({ name, pfpUrl, username });
-                setSounds(res.data.sounds);
-            })
-            .catch((err) => console.error(err));
-    }, [username]);
+        if (username) {
+            axios
+                .get(`${environment.API_URL}/api/v1/user/${username}`)
+                .then((res) => {
+                    const { name, pfpUrl } = res.data;
+                    let token;
+                    if (authUser) {
+                        token = authUser.token;
+                    }
+                    setUser({ ...res.data, token });
+                })
+                .catch((err) => console.error(err));
+        } else if (authUser) {
+            setUser(authUser);
+        }
+    }, [authUser, username]);
     return (
         <>
             <div className="main">
                 <div className="half">
                     <h1>{user && `${user.username}'s Profile`}</h1>
 
-                    <img src={user && user.pfpUrl} alt=">///<" />
+                    <img src={user?.pfpUrl || ""} alt=">///<" />
                 </div>
                 <div className="half">
                     <h1 style={{ height: "1rem" }}>Sounds</h1>
-                    {sounds &&
-                        sounds.map((sound: Sound, i: number) => {
-                            return (
-                                <Player
-                                    key={i}
-                                    sound={sound}
-                                    username={username}
-                                    setSounds={setSounds}
-                                    loggedInUserId={user?.userId || ""}
-                                    userToken={user?.token || ""}
-                                />
-                            );
-                        })}
+                    <SoundsList user={user} />
                 </div>
             </div>
         </>
