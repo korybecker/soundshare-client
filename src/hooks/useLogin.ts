dd63f4ab98694c8de092372cd82438fe678c8ae6
Kory Becker <68331399+korybecker@users.noreply.github.com>
2023-02-24 20:09:13 -0500

    changes

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,39 +1,43 @@
-import { useState } from 'react';
-import { useAuthContext } from './useAuthContext';
-import { environment } from '../environment';
+import { useState } from "react";
+import { useAuthContext } from "./useAuthContext";
+import { environment } from "../environment";
 
 export const useLogin = () => {
-	const [error, setError] = useState<string | null>(null);
-	const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string | null>(null);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
 
-	const { dispatch } = useAuthContext();
+    const { dispatch } = useAuthContext();
 
-	const login = async (formState: { email: string; password: string }) => {
-		const { email, password } = formState;
-		setIsLoading(true);
-		setError(null);
+    const login = async (formState: { email: string; password: string }) => {
+        const { email, password } = formState;
+        setIsLoading(true);
+        setError(null);
 
-		// try to login user
-		const response = await fetch(`${environment.API_URL}/api/v1/user/login`, {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({ email, password }),
-		});
-		const json = await response.json();
+        // try to login user
+        const response = await fetch(
+            `${environment.API_URL}/api/v1/user/login`,
+            {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ email, password }),
+            }
+        );
+        const json = await response.json();
 
-		// if failed, response.ok is false
-		if (!response.ok) {
-			setIsLoading(false);
-			setError(json.error.message);
-		} else {
-			// save user to local storage
-			localStorage.setItem('user', JSON.stringify(json));
+        // if failed, response.ok is false
+        if (!response.ok) {
+            setIsLoading(false);
+            setError(json.error.message);
+        } else {
+            // save user to local storage
+            localStorage.setItem("user", JSON.stringify(json));
 
-			// update auth context
-			dispatch({ type: 'LOGIN', payload: json });
+            // update auth context
+            dispatch({ type: "LOGIN", payload: json });
 
-			setIsLoading(false);
-		}
-	};
-	return { login, isLoading, error };
+            setIsLoading(false);
+        }
+        return json;
+    };
+    return { login, isLoading, error };
 };
--- src/hooks/useSignup.ts
+++ src/hooks/useSignup.ts
@@ -2,35 +2,57 @@ import { useState } from "react";
 import { useAuthContext } from "./useAuthContext";
 import { environment } from "../environment";
 
-import { User } from "../interfaces/User";
-
-import axios from "axios";
-
 export const useSignup = () => {
     const [error, setError] = useState(null);
     const [isLoading, setIsLoading] = useState<boolean>(false);
 
     const { dispatch } = useAuthContext();
 
-    const signup = async (data: FormData) => {
+    const signup = async (data: {
+        email: string;
+        name: string;
+        username: string;
+        password: string;
+        file: File | null;
+    }) => {
         setIsLoading(true);
         setError(null);
 
-        axios
-            .post(`${environment.API_URL}/api/v1/user/signup`, data, {
-                headers: {
-                    "Content-Type": "multipart/form-data",
-                },
-            })
-            .then((res) => {
-                const user: User = res.data;
-                dispatch({ type: "LOGIN", payload: user });
-                setIsLoading(false);
-            })
-            .catch((err) => {
-                setIsLoading(false);
-                console.error(err);
-            });
+        const { email, name, username, password, file } = data;
+
+        const formData = new FormData();
+        formData.append("email", email);
+        formData.append("name", name);
+        formData.append("username", username);
+        formData.append("password", password);
+        if (file) {
+            formData.append("file", file);
+        } else {
+            formData.append("file", "");
+        }
+        // try to sign up user
+        const response = await fetch(
+            `${environment.API_URL}/api/v1/user/signup`,
+            {
+                method: "POST",
+                body: formData,
+            }
+        );
+        const json = await response.json();
+
+        // if failed, response.ok is false
+        if (!response.ok) {
+            setIsLoading(false);
+            setError(json.error.message);
+        } else {
+            // save user to local storage
+
+            // update auth context
+            dispatch({ type: "LOGIN", payload: json });
+
+            setIsLoading(false);
+        }
+        return json;
     };
     return { signup, isLoading, error };
 };
