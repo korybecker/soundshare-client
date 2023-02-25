a5cf63897a4f95e4c846b44c3bf03914a8c5c7eb
Kory Becker <68331399+korybecker@users.noreply.github.com>
2023-02-25 14:16:35 -0500

    added edit sound state and functions

--- src/pages/Sounds.tsx
+++ src/pages/Sounds.tsx
@@ -1,12 +1,31 @@
 import "../App.css";
 
+import { useState } from "react";
+
 import { useAuthContext } from "../hooks/useAuthContext";
 
 import SoundsList from "../components/SoundsList";
+import EditSoundForm from "../components/EditSoundForm";
+
+import { Sound } from "../interfaces/Sound";
 
 const Sounds = () => {
     const { user } = useAuthContext();
+    const [showEditPopup, setShowEditPopup] = useState(false);
+    const [selectedSound, setSelectedSound] = useState<Sound | null>(null);
+    const [updated, setUpdated] = useState(false);
 
+    const handleEditClick = (e: React.MouseEvent, sound: Sound) => {
+        e.preventDefault();
+        setSelectedSound(sound);
+        setShowEditPopup(true);
+    };
+
+    const handlePopupClose = () => {
+        setSelectedSound(null);
+        setShowEditPopup(false);
+        setUpdated(!updated);
+    };
     return (
         <>
             <h1 style={{ textAlign: "center" }}>Sounds</h1>
@@ -16,7 +35,20 @@ const Sounds = () => {
                     isProfile={false}
                     loggedInUserId={user?.userId || ""}
                     username={user?.username || ""}
+                    onEditClick={handleEditClick}
+                    updatedSound={updated}
                 />
+                {showEditPopup && user && selectedSound && (
+                    <div className="editPopupContainer">
+                        <div className="editPopup">
+                            <EditSoundForm
+                                sound={selectedSound}
+                                user={user}
+                                onClose={handlePopupClose}
+                            />
+                        </div>
+                    </div>
+                )}
             </div>
         </>
     );
