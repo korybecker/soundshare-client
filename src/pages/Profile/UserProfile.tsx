c6ae334056ddedf19f764569ef9a6b5fa30e6bf5
Kory Becker <68331399+korybecker@users.noreply.github.com>
2023-02-24 20:06:44 -0500

    auth fault fixes

--- src/pages/Profile/UserProfile.tsx
+++ src/pages/Profile/UserProfile.tsx
@@ -1,46 +1,67 @@
-import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { environment } from "../../environment";
-import "./styles.css";
+import { useParams } from "react-router-dom";
 
-import axios from "axios";
-import SoundsList from "../../components/SoundsList";
 import { User } from "../../interfaces/User";
 import { useAuthContext } from "../../hooks/useAuthContext";
+import { environment } from "../../environment";
+
+import SoundsList from "../../components/SoundsList";
+
+import axios from "axios";
+import "./styles.css";
 
 const UserProfile = () => {
     const { username } = useParams();
     const { user: authUser } = useAuthContext();
 
     const [user, setUser] = useState<User | null>(null);
+    const [hasSounds, setHasSounds] = useState(false);
+
+    if (!(username || authUser)) {
+        return <div>no profile</div>;
+    }
+
     useEffect(() => {
-        if (username) {
-            axios
-                .get(`${environment.API_URL}/api/v1/user/${username}`)
-                .then((res) => {
-                    const { name, pfpUrl } = res.data;
-                    let token;
-                    if (authUser) {
-                        token = authUser.token;
-                    }
-                    setUser({ ...res.data, token });
-                })
-                .catch((err) => console.error(err));
-        } else if (authUser) {
-            setUser(authUser);
-        }
+        const fetchData = async () => {
+            try {
+                const res = await axios.get(
+                    `${environment.API_URL}/api/v1/user/${username}`
+                );
+                let token;
+                if (authUser) {
+                    token = authUser.token;
+                }
+                setUser(authUser ? { ...res.data, token } : res.data);
+                setHasSounds(res.data.hasSounds);
+            } catch (err) {
+                console.error(err);
+            }
+        };
+        fetchData();
     }, [authUser, username]);
+
     return (
         <>
             <div className="main">
                 <div className="half">
-                    <h1>{user && `${user.username}'s Profile`}</h1>
+                    <h1>{`${user?.username}'s Profile`}</h1>
 
                     <img src={user?.pfpUrl || ""} alt=">///<" />
                 </div>
                 <div className="half">
-                    <h1 style={{ height: "1rem" }}>Sounds</h1>
-                    <SoundsList user={user} />
+                    {hasSounds ? (
+                        <h1 style={{ height: "1rem" }}>Sounds</h1>
+                    ) : (
+                        <h1 style={{ height: "1rem" }}>No Sounds</h1>
+                    )}
+                    {user && (
+                        <SoundsList
+                            user={user}
+                            isProfile={true}
+                            loggedInUserId={authUser?.userId || ""}
+                            username={username || ""}
+                        />
+                    )}
                 </div>
             </div>
         </>
